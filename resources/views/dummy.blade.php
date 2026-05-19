caacbf0a384419f0902c886f2cd6552d011ae2a0
AkshaySocialscoop <onkar.socialscoop@gmail.com>
2026-05-19 17:53:55 +0530

    new update

--- resources/views/dummy.blade.php
+++ resources/views/dummy.blade.php
@@ -1,8 +1,15 @@
-@extends('layout.index')
-
-@section('title', 'Dashboard')
-
-@section('content')
-    <h1>Dashboard</h1>
-    <p>Welcome to TaskBoxOffice</p>
-@endsection
+@extends('layout.index')
+
+
+
+@section('title', 'Dashboard')
+
+
+
+@section('content')
+
+    <h1>Dashboard</h1>
+ 
+
+@endsection
+
