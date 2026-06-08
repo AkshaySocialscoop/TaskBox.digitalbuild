586bd93cd2bee7c3398934550ac0673b464a24f9
AkshaySocialscoop <onkar.socialscoop@gmail.com>
2026-06-08 18:22:35 +0530

    .

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -213,7 +213,7 @@ class TaskController extends Controller
             ]);
         }
 
-        return redirect('/user/dashboard')->with('success', 'Task status updated successfully!');
+        return redirect()->back()->with('success', 'Task status updated successfully!');
     }
 
 
